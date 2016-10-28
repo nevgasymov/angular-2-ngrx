58420e2dab92ebc310ff5d20e9518a360593f419
Sergii <snevgasymov@envionsoftware.com>
2016-10-28 17:15:08 +0300

    new component for the Hero details was added

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,6 +5,19 @@ export class Hero {
     name: string;
 }
 
+const HEROES: Hero[] = [
+    { id: 11, name: 'Mr. Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
+];
+
 @Component({
     selector: 'my-app',
     template:`
@@ -86,16 +99,3 @@ export class AppComponent {
         this.selectedHero = hero;
     }
 }
-
-const HEROES: Hero[] = [
-    { id: 11, name: 'Mr. Nice' },
-    { id: 12, name: 'Narco' },
-    { id: 13, name: 'Bombasto' },
-    { id: 14, name: 'Celeritas' },
-    { id: 15, name: 'Magneta' },
-    { id: 16, name: 'RubberMan' },
-    { id: 17, name: 'Dynama' },
-    { id: 18, name: 'Dr IQ' },
-    { id: 19, name: 'Magma' },
-    { id: 20, name: 'Tornado' }
-];
