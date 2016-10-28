a46c28f2aa15ec94749d2a642c9bfeef5e9f9b35
Sergii <snevgasymov@envionsoftware.com>
2016-10-28 18:40:17 +0300

    code refactoring

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { Hero } from './hero';
-import { HeroDetailComponent } from './hero-detail.component';
 
 const HEROES: Hero[] = [
     { id: 11, name: 'Mr. Nice' },
