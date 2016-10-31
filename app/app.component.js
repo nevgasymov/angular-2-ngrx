71331348aca87cf3b8f18c759fb0d4e250a8bd77
Sergii <snevgasymov@envionsoftware.com>
2016-10-31 13:11:19 +0200

    the ngOnInit method was added

--- app/app.component.js
+++ app/app.component.js
@@ -15,6 +15,13 @@ var AppComponent = (function () {
         this.heroService = heroService;
         this.title = 'Tour of Heroes';
     }
+    AppComponent.prototype.getHeroes = function () {
+        var _this = this;
+        this.heroService.getHeroes().then(function (heroes) { return _this.heroes = heroes; });
+    };
+    AppComponent.prototype.ngOnInit = function () {
+        this.getHeroes();
+    };
     AppComponent.prototype.onSelect = function (hero) {
         this.selectedHero = hero;
     };
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
 
@@ -75,6 +76,12 @@ export class AppComponent {
 
     constructor(private heroService: HeroService) { }
 
+    getHeroes(): void {
+        this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+    }
+    ngOnInit(): void {
+        this.getHeroes();
+    }
     onSelect(hero: Hero): void {
         this.selectedHero = hero;
     }
