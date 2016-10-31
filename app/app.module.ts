a67256848d4d376fc6be76f65014db5e4d5b98df
Sergii <snevgasymov@envionsoftware.com>
2016-10-31 22:30:10 +0200

    App Router was moved to own file, the heroes template and the heroes css were moved to the own files

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,34 +1,18 @@
 import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms';
-
 import { AppComponent }         from './app.component';
 import { DashboardComponent }   from './dashboard.component';
 import { HeroDetailComponent }  from './hero-detail.component';
 import { HeroesComponent }      from './heroes.component';
 import { HeroService }          from './hero.service';
-import { RouterModule }         from '@angular/router';
-
-// import { AppRoutingModule }     from './app-routing.module';
+import { AppRoutingModule }     from './app-routing.module';
 
 @NgModule({
     imports: [
         BrowserModule,
         FormsModule,
-        RouterModule.forRoot([
-            {
-                path: 'dashboard',
-                component: DashboardComponent
-            },
-            {
-                path: 'heroes',
-                component: HeroesComponent
-            },
-            {
-                path: 'detail/:id',
-                component: HeroDetailComponent
-            },
-        ])
+        AppRoutingModule
     ],
     declarations: [
         AppComponent,
@@ -36,10 +20,7 @@ import { RouterModule }         from '@angular/router';
         HeroDetailComponent,
         HeroesComponent
     ],
-    providers: [
-        HeroService
-    ],
+    providers: [ HeroService ],
     bootstrap: [ AppComponent ]
 })
-export class AppModule {
-}
+export class AppModule { }
