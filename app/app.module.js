abb735af9255116c695ddd3e9b656db9ddcd3c7d
Sergii <snevgasymov@envionsoftware.com>
2016-10-31 16:07:40 +0200

    the component for dashboard was added, and route was added for it

--- app/app.module.js
+++ app/app.module.js
@@ -15,6 +15,7 @@ var router_1 = require('@angular/router');
 var app_component_1 = require('./app.component');
 var hero_detail_component_1 = require('./hero-detail.component');
 var heroes_component_1 = require('./heroes.component');
+var dashboard_component_1 = require('./dashboard.component');
 var hero_service_1 = require('./hero.service');
 var AppModule = (function () {
     function AppModule() {
@@ -23,18 +24,24 @@ var AppModule = (function () {
         core_1.NgModule({
             imports: [
                 platform_browser_1.BrowserModule,
-                forms_1.FormsModule
-            ],
-            declarations: [
-                app_component_1.AppComponent,
-                hero_detail_component_1.HeroDetailComponent,
-                heroes_component_1.HeroesComponent,
+                forms_1.FormsModule,
                 router_1.RouterModule.forRoot([
+                    {
+                        path: '',
+                        redirectTo: '/dashboard',
+                        pathMatch: 'full'
+                    },
                     {
                         path: 'heroes',
                         component: heroes_component_1.HeroesComponent
-                    }
+                    },
                 ])
+            ],
+            declarations: [
+                app_component_1.AppComponent,
+                dashboard_component_1.DashboardComponent,
+                hero_detail_component_1.HeroDetailComponent,
+                heroes_component_1.HeroesComponent
             ],
             providers: [
                 hero_service_1.HeroService
@@ -46,10 +53,4 @@ var AppModule = (function () {
     return AppModule;
 }());
 exports.AppModule = AppModule;
-router_1.RouterModule.forRoot([
-    {
-        path: 'heroes',
-        component: heroes_component_1.HeroesComponent
-    }
-]);
 //# sourceMappingURL=app.module.js.map
