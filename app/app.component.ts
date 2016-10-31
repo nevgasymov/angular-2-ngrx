f52f9eaa5d1c2064528ca1494f0158237ba5f167
Sergii <snevgasymov@envionsoftware.com>
2016-10-31 12:36:19 +0200

    the providers array property was added for AppComponent, the Promise was added in getHeroes method

--- app/app.component.ts
+++ app/app.component.ts
@@ -64,7 +64,8 @@ import { HeroService } from './hero.service';
             margin-right: .8em;
             border-radius: 4px 0 0 4px;
         }
-    `]
+    `],
+    providers: [HeroService]
 })
 
 export class AppComponent {
