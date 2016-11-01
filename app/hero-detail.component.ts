a9cff5e912737a9e7223de02c91d6620d908da13
Sergii <snevgasymov@envionsoftware.com>
2016-11-01 10:35:38 +0200

    styles for dashboard and for hero-detail were added

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -8,10 +8,10 @@ import { HeroService } from './hero.service';
 @Component({
     moduleId: module.id,
     selector: 'my-hero-detail',
-    templateUrl: 'hero-detail.component.html'
+    templateUrl: 'hero-detail.component.html',
+    styleUrls: [ 'hero-detail.component.css' ]
 })
 export class HeroDetailComponent implements OnInit {
-    @Input() hero: Hero;
 
     constructor(
         private heroService: HeroService,
